3f02381b6d3e867f5e76b223e5cb0063ebd2b431
1549469775 <1549469775@qq.com>
2019-11-13 09:19:50 +0800

    fasf

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,57 +2,69 @@ const path = require('path');
 module.exports = {
   mode: 'universal',
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
     // process.env.npm_package_name || 
     title: 'Beer',
-    meta: [
-      {charset: 'utf-8'},
-      {name: 'viewport', content: 'width=device-width, initial-scale=1, shrink-to-fit=no'},
-      {hid: 'description', name: 'description', content: process.env.npm_package_description || ''}
+    meta: [{
+        charset: 'utf-8'
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1, shrink-to-fit=no'
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || ''
+      }
     ],
-    link: [
-      {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
-    ]
+    link: [{
+      rel: 'icon',
+      type: 'image/x-icon',
+      href: '/favicon.ico'
+    }]
   },
   // https://www.cnblogs.com/bgwhite/p/9403285.html
   // https://zh.nuxtjs.org/api/configuration-router#base
   router: {
     // 相对路径打包
-    base: process.env.NODE_ENV==='production'&&process.env.BASE?process.env.BASE:'/'
+    base: process.env.NODE_ENV === 'production' && process.env.BASE ? process.env.BASE : '/'
   },
-  generate:{
-    dir:'docs'
+  generate: {
+    dir: 'docs'
   },
   env: {
     baseUrl: process.env.BASE_URL || 'http://localhost:3000'
   },
   /*
-  ** Customize the progress-bar color
-  */
-  loading: {color: '#fff'},
+   ** Customize the progress-bar color
+   */
+  loading: {
+    color: '#fff'
+  },
   /*
-  ** Global CSS
-  */
+   ** Global CSS
+   */
   css: [
     'reset-css/reset.css',
   ],
   /*
-  ** Plugins to load before mounting the App
-  */
+   ** Plugins to load before mounting the App
+   */
   plugins: [
     '~/plugins/global',
     '~/plugins/axios',
     '~/plugins/global-components'
   ],
   /*
-  ** Nuxt.js dev-modules
-  */
+   ** Nuxt.js dev-modules
+   */
   devModules: [],
   /*
-  ** Nuxt.js modules
-  */
+   ** Nuxt.js modules
+   */
   modules: [
     '@nuxtjs/style-resources',
     // 使用bootstrap-vue
@@ -62,12 +74,14 @@ module.exports = {
   //https://axios.nuxtjs.org/setup
   axios: {
     // proxyHeaders: false
-    retry: {retries: 3},
-    proxy: process.env.NODE_ENV==='production'?false:true,
-    baseURL:process.env.NODE_ENV==='production'&&process.env.BASE?'http://47.244.111.211:3303':''
+    retry: {
+      retries: 3
+    },
+    proxy: process.env.NODE_ENV === 'production' ? false : true,
+    baseURL: process.env.NODE_ENV === 'production' && process.env.BASE ? 'https://beer.poorman.top:3303' : ''
   },
   proxy: {
-    '/api': 'http://47.244.111.211:3303'
+    '/api': 'https://beer.poorman.top:3303'
   },
   styleResources: {
     // your settings here
@@ -77,12 +91,12 @@ module.exports = {
     stylus: []
   },
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     /*
-    ** You can extend webpack config here
-    */
+     ** You can extend webpack config here
+     */
     extend(config, ctx) {
       config.resolve.alias['@pages'] = path.resolve(__dirname, 'pages');
       config.resolve.alias['@assets'] = path.resolve(__dirname, 'assets');
